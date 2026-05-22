39dbd3da2634a2b48d0e7ed7b6d0e335479bf5f5
Yoadgo <yoad9852@gmail.com>
2026-05-22 19:12:38 +0300

    עדכון לגרסא פלאפון

--- js/app.js
+++ js/app.js
@@ -25,10 +25,7 @@ const App = (() => {
     const logoHTML = `
       <div class="sidebar-logo">
         <div class="logo-icon">
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"/>
-            <polyline points="17 6 23 6 23 12"/>
-          </svg>
+          <img src="assets/logo.png" alt="לוגו" />
         </div>
         <div>
           <div class="logo-text">FinanceAPP</div>
